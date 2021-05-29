8c39917a5dde47cc18109c552265f7d83ceb27dc
a1709 <41358463+yga1709@users.noreply.github.com>
2021-05-30 02:08:07 +0900

    add : download process

--- yd.sh
+++ yd.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 path=~/Downloads
 today=$(date "+%Y%m%d")
+
+# The process of download a youtube movie
+youtube-dl -o ${path}/music_${today}.webm -f 251 $1
+youtube-dl -o "${path}/movie_${today}_%(title)s-%(id)s.%(ext)s" -f 248 $1
