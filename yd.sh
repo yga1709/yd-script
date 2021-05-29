da900dd6d736c8a6c13b4b0f6b2c5b5667051ae9
a1709 <41358463+yga1709@users.noreply.github.com>
2021-05-30 02:12:09 +0900

    add : Video and audio linkage

--- yd.sh
+++ yd.sh
@@ -2,6 +2,11 @@
 path=~/Downloads
 today=$(date "+%Y%m%d")
 
-# The process of download a youtube movie
+# The process of download a youtube movie. The format num is for my environment.
 youtube-dl -o ${path}/music_${today}.webm -f 251 $1
 youtube-dl -o "${path}/movie_${today}_%(title)s-%(id)s.%(ext)s" -f 248 $1
+
+echo $music
+echo $movie
+echo $title
+ffmpeg -i $music -i $movie -c:v copy -c:a copy ${path}/${title}
